b1061becb62d6722afcabb1b2f28afd5a07b5ca3
Volodymyr Ananiev <volodymyr.ananiev@gmail.com>
2021-11-01 16:49:45 +0200

    More error checking
    Signed-off-by: Volodymyr Ananiev <volodymyr.ananiev@gmail.com>

--- main/pipeline_passthrough.c
+++ main/pipeline_passthrough.c
@@ -15,35 +15,48 @@ static audio_element_handle_t i2s_stream_writer, i2s_stream_reader;
 
 esp_err_t pipeline_passthrough_start(audio_event_iface_handle_t evt)
 {
-    ESP_LOGI(TAG, "[ 2 ] start");
+    ESP_LOGI(TAG, "[1] start");
     audio_pipeline_cfg_t pipeline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
     pipeline = audio_pipeline_init(&pipeline_cfg);
+    if (pipeline == NULL) {
+        ESP_LOGE(TAG, "error audio_pipeline_init");
+        return ESP_FAIL;
+    }
 
-    ESP_LOGI(TAG, "[3.1] Create i2s stream to write data to codec chip");
+    ESP_LOGI(TAG, "[2] Create i2s stream to write data to codec chip");
     i2s_stream_cfg_t i2s_cfg = I2S_STREAM_CFG_DEFAULT();
     i2s_cfg.type = AUDIO_STREAM_WRITER;
     i2s_stream_writer = i2s_stream_init(&i2s_cfg);
+    if (i2s_stream_writer == NULL) {
+        ESP_LOGE(TAG, "error init i2s_stream_writer");
+        return ESP_FAIL;
+    }
 
-    ESP_LOGI(TAG, "[3.2] Create i2s stream to read data from codec chip");
+    ESP_LOGI(TAG, "[3] Create i2s stream to read data from codec chip");
     i2s_stream_cfg_t i2s_cfg_read = I2S_STREAM_CFG_DEFAULT();
     i2s_cfg_read.type = AUDIO_STREAM_READER;
     i2s_stream_reader = i2s_stream_init(&i2s_cfg_read);
+    if (i2s_stream_reader == NULL) {
+        ESP_LOGE(TAG, "error init i2s_stream_reader");
+        return ESP_FAIL;
+    }
 
-    ESP_LOGI(TAG, "[3.3] Register all elements to audio pipeline");
-    audio_pipeline_register(pipeline, i2s_stream_reader, "i2s_read");
-    audio_pipeline_register(pipeline, i2s_stream_writer, "i2s_write");
+    ESP_LOGI(TAG, "[4] Register all elements to audio pipeline");
+    ESP_ERROR_CHECK(audio_pipeline_register(pipeline, i2s_stream_reader, "i2s_read"));
+    ESP_ERROR_CHECK(audio_pipeline_register(pipeline, i2s_stream_writer, "i2s_write"));
 
-    ESP_LOGI(TAG, "[3.4] Link it together [codec_chip]-->i2s_stream_reader-->i2s_stream_writer-->[codec_chip]");
+    ESP_LOGI(TAG, "[5] Link it together [codec_chip]-->i2s_stream_reader-->i2s_stream_writer-->[codec_chip]");
     const char *link_tag[2] = {"i2s_read", "i2s_write"};
-    audio_pipeline_link(pipeline, &link_tag[0], 2);
+    ESP_ERROR_CHECK(audio_pipeline_link(pipeline, link_tag, 2));
 
-    ESP_LOGI(TAG, "[4.1] Listening event from all elements of pipeline");
-    audio_pipeline_set_listener(pipeline, evt);
+    ESP_LOGI(TAG, "[6] Listening event from all elements of pipeline");
+    ESP_ERROR_CHECK(audio_pipeline_set_listener(pipeline, evt));
 
-    ESP_LOGI(TAG, "[ 5 ] Start audio_pipeline");
-    audio_pipeline_run(pipeline);
+    ESP_LOGI(TAG, "[7] Start audio_pipeline");
+    ESP_ERROR_CHECK(audio_pipeline_run(pipeline));
 
-    esp_event_post_to(pipeline_event_loop, PIPELINE_EVENTS, PIPELINE_PASSTHROUGH_STARTED, NULL, 0, portMAX_DELAY);
+    ESP_ERROR_CHECK(esp_event_post_to(pipeline_event_loop, PIPELINE_EVENTS,
+                                      PIPELINE_PASSTHROUGH_STARTED, NULL, 0, portMAX_DELAY));
 
     return ESP_OK;
 }
